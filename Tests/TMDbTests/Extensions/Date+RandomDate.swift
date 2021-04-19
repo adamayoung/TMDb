5053d52c8b42d4934a7e262e7b7a349a66f2dc24
Adam Young <me@adam-young.co.uk>
2021-04-19 15:51:38 +0100

    Fixed linting issues

--- Tests/TMDbTests/Extensions/Date+RandomDate.swift
+++ Tests/TMDbTests/Extensions/Date+RandomDate.swift
@@ -4,7 +4,7 @@ extension Date {
 
     static var random: Self {
         let date = Date().timeIntervalSince1970
-        let timeInterval = Double.random(in: 1118839159...now)
+        let timeInterval = Double.random(in: 1118839159...date)
         return Date(timeIntervalSince1970: timeInterval)
     }
 
