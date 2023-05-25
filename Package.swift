f88584f2207d67262671aa2f2cf568f6d9aa95eb
Adam Young <me@adam-young.co.uk>
2023-05-25 23:05:52 +0100

    CONFIG: Remove swiftlint plugin

--- Package.swift
+++ Package.swift
@@ -9,7 +9,10 @@ let package = Package(
     defaultLocalization: "en",
 
     platforms: [
-        .macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)
+        .macOS(.v12),
+        .iOS(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
 
     products: [
@@ -25,9 +28,6 @@ let package = Package(
             name: "TMDb",
             resources: [
                 .process("Resources")
-            ],
-            plugins: [
-                .plugin(name: "SwiftLintPlugin")
             ]
         ),
         .testTarget(
@@ -35,9 +35,6 @@ let package = Package(
             dependencies: ["TMDb"],
             resources: [
                 .process("Resources")
-            ],
-            plugins: [
-                .plugin(name: "SwiftLintPlugin")
             ]
         ),
         .testTarget(
@@ -45,32 +42,7 @@ let package = Package(
             dependencies: ["TMDb"],
             resources: [
                 .process("Resources")
-            ],
-            plugins: [
-                .plugin(name: "SwiftLintPlugin")
             ]
-        ),
-
-        .plugin(
-            name: "SwiftLintPlugin",
-            capability: .buildTool(),
-            dependencies: ["SwiftLintBinary"]
-        ),
-        .plugin(
-            name: "SwiftLint",
-            capability: .command(
-                intent: .custom(
-                    verb: "swiftlint",
-                    description: "SwiftLint."
-                )
-            ),
-            dependencies: ["SwiftLintBinary"]
-        ),
-
-        .binaryTarget(
-            name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.51.0/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "9fbfdf1c2a248469cfbe17a158c5fbf96ac1b606fbcfef4b800993e7accf43ae"
         )
     ]
 )
