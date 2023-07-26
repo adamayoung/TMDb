1a77ab262db7c5dc5ed1a894b2bc0d61800a5c21
Adam Young <me@adam-young.co.uk>
2023-07-27 00:09:39 +0100

    Add movie integration tests (#98)

--- Tests/TMDbIntegrationTests/Extensions/XCTestCase+Environment.swift
+++ Tests/TMDbIntegrationTests/Extensions/XCTestCase+Environment.swift
@@ -7,7 +7,10 @@ extension XCTestCase {
     }
 
     static var tmdbAPIKey: String {
-        guard let apiKey = ProcessInfo.processInfo.environment["TMDB_API_KEY"] else {
+        guard
+            let apiKey = ProcessInfo.processInfo.environment["TMDB_API_KEY"],
+            !apiKey.isEmpty
+        else {
             preconditionFailure("TMDb configuration API key not set. Use TMDB_API_KEY environment variable.")
         }
 
