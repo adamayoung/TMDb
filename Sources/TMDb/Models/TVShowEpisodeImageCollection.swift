cdcebff612af8a7b0b69b8ceaf2e5095fca9cdc7
Adam Young <me@adam-young.co.uk>
2023-08-03 11:23:08 +0100

    Add TVShowEpisodeServiceTests

--- Sources/TMDb/Models/TVShowEpisodeImageCollection.swift
+++ Sources/TMDb/Models/TVShowEpisodeImageCollection.swift
@@ -6,7 +6,7 @@ import Foundation
 public struct TVShowEpisodeImageCollection: Identifiable, Decodable, Equatable, Hashable {
 
     ///
-    /// Person identifier.
+    /// Episode identifier.
     ///
     public let id: Int
 
