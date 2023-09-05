66e8132c7e38bc025830879ee16817a5264bf1c1
Adam Young <me@adam-young.co.uk>
2023-09-05 15:46:00 +0100

    CONFIG: Conditionally depend on DocC

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import class Foundation.ProcessInfo
 import PackageDescription
 
 let package = Package(
@@ -17,10 +18,6 @@ let package = Package(
 
     products: [
         .library(name: "TMDb", targets: ["TMDb"])
-    ],
-
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0")
     ],
 
     targets: [
@@ -43,3 +40,9 @@ let package = Package(
         )
     ]
 )
+
+if ProcessInfo.processInfo.environment["SWIFTCI_DOCC"] == "1" {
+    package.dependencies += [
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0")
+    ]
+}
