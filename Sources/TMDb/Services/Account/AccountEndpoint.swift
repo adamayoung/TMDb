cf6bede25e521ee5a6bfe30105c61286353db7ea
Adam Young <me@adam-young.co.uk>
2024-03-08 13:57:47 +0000

    Add account details endpoint

---
+++ Sources/TMDb/Services/Account/AccountEndpoint.swift
@@ -0,0 +1,44 @@
+//
+//  AccountEndpoint.swift
+//  TMDb
+//
+//  Copyright © 2024 Adam Young.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an AS IS BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+enum AccountEndpoint {
+
+    case details(session: Session)
+
+}
+
+extension AccountEndpoint: Endpoint {
+
+    private static let basePath = URL(string: "/account")!
+
+    private enum QueryItemName {
+        static let sessionID = "session_id"
+    }
+
+    var path: URL {
+        switch self {
+        case let .details(session):
+            Self.basePath
+                .appendingQueryItem(name: QueryItemName.sessionID, value: session.sessionID)
+        }
+    }
+
+}
