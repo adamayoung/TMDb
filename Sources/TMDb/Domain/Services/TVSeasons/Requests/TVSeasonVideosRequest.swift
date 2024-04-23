dcfb022fb6f7c32c9b70acb0070a478710b42489
Adam Young <me@adam-young.co.uk>
2024-04-23 18:42:31 +0100

    TVEpisode refactor

---
+++ Sources/TMDb/Domain/Services/TVSeasons/Requests/TVSeasonVideosRequest.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adam Young on 23/04/2024.
+//
+
+import Foundation
---
+++ Tests/TMDbTests/Domain/Services/TVSeasons/Requests/TVSeasonVideosRequestTests.swift
@@ -0,0 +1,68 @@
+//
+//  TVSeasonVideosRequestTests.swift
+//  TMDb
+//
+//  Copyright © 2024 Adam Young.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an AS IS BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+@testable import TMDb
+import XCTest
+
+final class TVSeasonVideosRequestTests: XCTestCase {
+
+    func testPath() {
+        let request = TVSeasonVideosRequest(seasonNumber: 2, tvSeriesID: 3)
+
+        XCTAssertEqual(request.path, "/tv/3/season/2/videos")
+    }
+
+    func testQueryItemsWhenLanguageCodeIsNilQueryItemsAreEmpty() {
+        let request = TVSeasonVideosRequest(seasonNumber: 2, tvSeriesID: 3)
+
+        XCTAssertTrue(request.queryItems.isEmpty)
+    }
+
+    func testQueryItemsWhenLanguageCodeQueryItemsHasLanguageCode() {
+        let request = TVSeasonVideosRequest(seasonNumber: 2, tvSeriesID: 3, languageCode: "en")
+
+        XCTAssertEqual(request.queryItems.count, 1)
+        XCTAssertEqual(request.queryItems["include_video_language"], "en,null")
+    }
+
+    func testMethodIsGet() {
+        let request = TVSeasonVideosRequest(seasonNumber: 2, tvSeriesID: 3)
+
+        XCTAssertEqual(request.method, .get)
+    }
+
+    func testHeadersIsEmpty() {
+        let request = TVSeasonVideosRequest(seasonNumber: 2, tvSeriesID: 3)
+
+        XCTAssertTrue(request.headers.isEmpty)
+    }
+
+    func testBodyIsNil() {
+        let request = TVSeasonVideosRequest(seasonNumber: 2, tvSeriesID: 3)
+
+        XCTAssertNil(request.body)
+    }
+
+    func testSerialiserIsTMDbJSON() {
+        let request = TVSeasonVideosRequest(seasonNumber: 2, tvSeriesID: 3)
+
+        XCTAssertTrue(request.serialiser is TMDbJSONSerialiser)
+    }
+
+}
