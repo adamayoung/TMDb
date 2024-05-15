3dd38a7ca46931cb264b88dd98ace1101bc4d3ad
Adam Young <me@adam-young.co.uk>
2024-05-15 15:42:44 +0100

    Break up MovieServiceTests and TVSeriesTests

---
+++ Tests/TMDbTests/Domain/Services/Movies/MovieServiceDetailsTests.swift
@@ -0,0 +1,81 @@
+//
+//  MovieServiceDetailsTests.swift
+//  TMDb
+//
+//  Copyright © 2024 Adam Young.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an AS IS BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+@testable import TMDb
+import XCTest
+
+final class MovieServiceDetailsTests: XCTestCase {
+
+    var service: MovieService!
+    var apiClient: MockAPIClient!
+
+    override func setUp() {
+        super.setUp()
+        apiClient = MockAPIClient()
+        service = MovieService(apiClient: apiClient)
+    }
+
+    override func tearDown() {
+        apiClient = nil
+        service = nil
+        super.tearDown()
+    }
+
+    func testDetailsReturnsMovie() async throws {
+        let expectedResult = Movie.thorLoveAndThunder
+        let movieID = expectedResult.id
+        apiClient.addResponse(.success(expectedResult))
+        let expectedRequest = MovieRequest(id: movieID, language: nil)
+
+        let result = try await service.details(forMovie: movieID)
+
+        XCTAssertEqual(result, expectedResult)
+        XCTAssertEqual(apiClient.lastRequest as? MovieRequest, expectedRequest)
+    }
+
+    func testDetailsWithLanguageReturnsMovie() async throws {
+        let expectedResult = Movie.thorLoveAndThunder
+        let movieID = expectedResult.id
+        let language = "en"
+        apiClient.addResponse(.success(expectedResult))
+        let expectedRequest = MovieRequest(id: movieID, language: language)
+
+        let result = try await service.details(forMovie: movieID, language: language)
+
+        XCTAssertEqual(result, expectedResult)
+        XCTAssertEqual(apiClient.lastRequest as? MovieRequest, expectedRequest)
+    }
+
+    func testDetailsWhenErrorsThrowsError() async throws {
+        let movieID = 1
+        apiClient.addResponse(.failure(.unknown))
+
+        var error: Error?
+        do {
+            _ = try await service.details(forMovie: movieID)
+        } catch let err {
+            error = err
+        }
+
+        let tmdbAPIError = try XCTUnwrap(error as? TMDbError)
+
+        XCTAssertEqual(tmdbAPIError, .unknown)
+    }
+
+}
