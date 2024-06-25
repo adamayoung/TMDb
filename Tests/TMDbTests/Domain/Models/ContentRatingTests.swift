621cc679ecf8dfc658f8311c2abd62adc0c82442
chickdan <daniel.chick@hotmail.com>
2024-06-25 17:29:56 -0500

    Add mocks and tests

---
+++ Tests/TMDbTests/Domain/Models/ContentRatingTests.swift
@@ -0,0 +1,38 @@
+//
+//  CertificationTests.swift
+//  TMDb
+//
+//  Copyright © 2024 Adam Young.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an AS IS BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+@testable import TMDb
+import XCTest
+
+final class ContentRatingTests: XCTestCase {
+
+    func testDecodeReturnsContentRating() throws {
+        let result = try JSONDecoder.theMovieDatabase.decode(ContentRating.self, fromResource: "content-rating")
+
+        XCTAssertEqual(result.descriptors, contentRating.descriptors)
+        XCTAssertEqual(result.countryCode, contentRating.countryCode)
+        XCTAssertEqual(result.rating, contentRating.rating)
+    }
+
+    private let contentRating = ContentRating(
+        descriptors: [],
+        countryCode: "US",
+        rating: "TV-14"
+    )
+}
---
+++ Tests/TMDbTests/Domain/Models/ContentRatingsTests.swift
@@ -0,0 +1,153 @@
+//
+//  CertificationTests.swift
+//  TMDb
+//
+//  Copyright © 2024 Adam Young.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an AS IS BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+@testable import TMDb
+import XCTest
+
+final class ContentRatingsTests: XCTestCase {
+
+    func testDecodeReturnsContentRatingResult() throws {
+        let result = try JSONDecoder.theMovieDatabase.decode(ContentRatingResult.self, fromResource: "content-ratings")
+
+        XCTAssertEqual(result, contentRatings)
+    }
+
+    // swiftlint:disable line_length
+    private let contentRatings = ContentRatingResult(
+        results: [
+            ContentRating(
+                descriptors: [],
+                countryCode: "US",
+                rating: "TV-14"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "AU",
+                rating: "M"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "RU",
+                rating: "12+"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "GB",
+                rating: "15"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "BR",
+                rating: "12"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "HU",
+                rating: "12"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "PH",
+                rating: "G"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "MX",
+                rating: "A"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "PT",
+                rating: "NR"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "ES",
+                rating: "16"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "FR",
+                rating: "16"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "CA",
+                rating: "PG"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "NL",
+                rating: "6"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "DE",
+                rating: "12"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "DE",
+                rating: "16"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "AU",
+                rating: "PG"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "KR",
+                rating: "15"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "AT",
+                rating: "12"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "CH",
+                rating: "12"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "PL",
+                rating: "16"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "HU",
+                rating: "16"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "CZ",
+                rating: "15+"
+            ),
+            ContentRating(
+                descriptors: [],
+                countryCode: "RO",
+                rating: "15"
+            ),
+        ],
+        id: 8592
+    )
+    // swiftlint:enable line_length
+}
