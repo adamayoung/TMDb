7e5ebb63b895b8eb4c079e985f37d98c64030286
MasterBlaster Code <odhlasenia_vlady0x@icloud.com>
2025-05-03 16:31:22 +0200

    Create OnTheAirTVSeriesRequest.swift

---
+++ Sources/TMDb/Domain/Services/TVSeries/Requests/OnTheAirTVSeriesRequest.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+final class OnTheAirTVSeriesRequest: DecodableAPIRequest<TVSeriesPageableList> {
+
+    init(page: Int? = nil, language: String? = nil) {
+        let path = "/tv/on_the_air"
+        let queryItems = APIRequestQueryItems(page: page, language: language)
+
+        super.init(path: path, queryItems: queryItems)
+    }
+
+}
