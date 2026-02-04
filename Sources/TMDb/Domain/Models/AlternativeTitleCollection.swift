5c8d5a8643e0182e4756ee0d374848d7fc7c5c6c
Adam Young <me@adam-young.co.uk>
2026-02-04 18:51:39 +0000

    Tidy up

--- Sources/TMDb/Domain/Models/AlternativeTitleCollection.swift
+++ Sources/TMDb/Domain/Models/AlternativeTitleCollection.swift
@@ -36,7 +36,7 @@ public struct AlternativeTitleCollection: Codable, Equatable, Hashable, Sendable
 
 }
 
-extension AlternativeTitleCollection {
+public extension AlternativeTitleCollection {
 
     private enum CodingKeys: String, CodingKey {
         case id
@@ -44,7 +44,7 @@ extension AlternativeTitleCollection {
         case results
     }
 
-    public init(from decoder: Decoder) throws {
+    init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
         self.id = try container.decode(Int.self, forKey: .id)
@@ -59,7 +59,7 @@ extension AlternativeTitleCollection {
         }
     }
 
-    public func encode(to encoder: Encoder) throws {
+    func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
 
         try container.encode(id, forKey: .id)
